b702f5ff60d210518891b30d2475a71d35a86f6f
faizal <128202217+mfaizal1504@users.noreply.github.com>
2025-03-06 22:51:46 +0800

    Create basic grid

---
+++ script.js
@@ -0,0 +1,22 @@
+const sketchboard = document.querySelector("#sketch-board");
+if (sketchboard === null) {
+    console.log("Cannot find that");
+}
+
+const iCount = 20;
+
+for (i = 0; i < iCount; i++) {
+    // create 'major' grid
+    let majorGrid = document.createElement("div");
+    majorGrid.classList.add("major-grid");
+
+    //create 'minor' grid
+    for (let j = 0; j < iCount; j++) {
+        let minorGrid = document.createElement("div");
+        minorGrid.classList.add("minor-grid");
+        majorGrid.appendChild(minorGrid);
+    };
+    
+    sketchboard.appendChild(majorGrid);    
+}
+
