e8d2d475b116abd2b9736598eeeae2713a47a826
faizal <128202217+mfaizal1504@users.noreply.github.com>
2025-03-11 13:07:30 +0800

    Remove grid numberer

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ sketchboard.style.width = `${BOARD_SIZE}px`;
 sketchboard.style.height = `${BOARD_SIZE}px`;
 
 // number of grid for each side (will be made changeable later)
-const sideCount = 11;
+const sideCount = 64;
 
 
 // calculate the size for each grid and number of grids
@@ -19,7 +19,7 @@ const gridCount = sideCount * sideCount;
 
 for (let i = 0; i < gridCount; i++) {
     const minorGrid = document.createElement('div');
-    minorGrid.textContent = `${i+1}`;
+    // minorGrid.textContent = `${i+1}`;
     minorGrid.classList.add('minor-grid');
     minorGrid.style.minWidth = `${gridSize}px`;
     minorGrid.style.height = `${gridSize}px`;
